cf97421e600de8a2e6d43725559e4afd4e9ac237
Max Kofler <kofler.max.dev@gmail.com>
2023-09-23 12:33:09 +0200

    Add /u/user endpoints

---
+++ src/velocity/endpoints/u/u_group.rs
@@ -0,0 +1,148 @@
+use reqwest::Method;
+use serde::{Deserialize, Serialize};
+
+use crate::{error::VelocityError, Velocity};
+
+use crate::{GID, UID};
+
+use super::u_user::Permission;
+
+impl Velocity {
+    /// Provides group information
+    /// # Arguments
+    /// * `gid` - The group id of the group to inform about
+    pub async fn group_info(&self, gid: GID) -> Result<GroupInfo, VelocityError> {
+        let authkey = self.get_authkey()?;
+
+        let request = UGroupPOSTReq {
+            authkey: authkey.key(),
+            gid,
+        };
+
+        Ok(self
+            .request_json::<UGroupPOSTReq, GroupInfo>(Method::POST, "/u/group", &request)
+            .await?
+            .response)
+    }
+
+    /// Creates a new group within the Velocity system
+    /// # Arguments
+    /// * `parent_gid` - The `gid` of the parent group this new group should be a part of
+    /// * `name` - A unique name for the new group within the parent group
+    pub async fn group_create(
+        &self,
+        parent_gid: GID,
+        name: &str,
+    ) -> Result<UGroupPUTRes, VelocityError> {
+        let authkey = self.get_authkey()?;
+
+        let request = UGroupPUTReq {
+            authkey: authkey.key(),
+            parent_gid,
+            name,
+        };
+
+        Ok(self
+            .request_json::<UGroupPUTReq, UGroupPUTRes>(Method::PUT, "/u/group", &request)
+            .await?
+            .response)
+    }
+
+    /// Removes a group from the Velocity system
+    /// # Arguments
+    /// * `gid` - The `gid` of the group to remove
+    pub async fn group_remove(&self, gid: GID) -> Result<(), VelocityError> {
+        let authkey = self.get_authkey()?;
+
+        let request = UGroupDELETEReq {
+            authkey: authkey.key(),
+            gid,
+        };
+
+        self.request(Method::DELETE, "/u/group", &request).await?;
+
+        Ok(())
+    }
+
+    /// List all groups visible to the current user
+    pub async fn group_list(&self) -> Result<Vec<UGroupListPOSTRes>, VelocityError> {
+        let authkey = self.get_authkey()?;
+
+        let request = UGroupListPOSTReq {
+            authkey: authkey.key(),
+        };
+
+        #[derive(Deserialize)]
+        struct Res {
+            groups: Vec<UGroupListPOSTRes>,
+        }
+
+        Ok(self
+            .request_json::<UGroupListPOSTReq, Res>(Method::POST, "/u/group/list", &request)
+            .await?
+            .response
+            .groups)
+    }
+}
+
+/// A struct providing information about a group
+#[derive(Deserialize, Debug)]
+pub struct GroupInfo {
+    pub name: String,
+    pub gid: GID,
+    pub parent_gid: GID,
+    pub memberships: Vec<GroupMembership>,
+}
+
+/// Describes a membership
+#[derive(Deserialize, Debug)]
+pub struct GroupMembership {
+    pub uid: UID,
+    pub name: String,
+    pub permissions: Vec<Permission>,
+}
+
+/// `/u/group - POST` Request structure
+#[derive(Serialize)]
+struct UGroupPOSTReq<'a> {
+    authkey: &'a str,
+    gid: GID,
+}
+
+/// `/u/group - PUT` Request structure
+#[derive(Serialize)]
+struct UGroupPUTReq<'a> {
+    authkey: &'a str,
+    name: &'a str,
+    parent_gid: GID,
+}
+
+/// `/u/group - PUT` Response structure
+#[derive(Deserialize, Debug)]
+pub struct UGroupPUTRes {
+    pub gid: GID,
+    pub parent_gid: GID,
+    pub name: String,
+}
+
+/// `/u/group - DELETE` Request structure
+#[derive(Serialize)]
+struct UGroupDELETEReq<'a> {
+    authkey: &'a str,
+    gid: GID,
+}
+
+/// `/u/group/list - POST` Request structure
+#[derive(Serialize)]
+struct UGroupListPOSTReq<'a> {
+    authkey: &'a str,
+}
+
+/// `/u/group/list - POST` Response structure
+#[derive(Deserialize, Debug)]
+pub struct UGroupListPOSTRes {
+    pub name: String,
+    pub gid: GID,
+    pub parent_gid: GID,
+    pub permissions: Vec<Permission>,
+}
---
+++ src/velocity/endpoints/u/u_user.rs
@@ -0,0 +1,150 @@
+use reqwest::Method;
+use serde::{Deserialize, Serialize};
+
+use crate::{error::VelocityError, Velocity};
+
+use super::{GID, PID, UID};
+
+impl Velocity {
+    /// Creates a new user using the provided credentials
+    /// # Arguments
+    /// * `username` - The unique username to use for the newly created user
+    /// * `password` - The password to use for the newly created user
+    /// # Return
+    /// The `uid` of the new user
+    pub async fn user_create(&self, username: &str, password: &str) -> Result<UID, VelocityError> {
+        let authkey = self.get_authkey()?;
+
+        let request = UUserPUTReq {
+            authkey: authkey.key(),
+            name: username,
+            password,
+        };
+
+        Ok(self
+            .request_json::<UUserPUTReq, UUserPUTRes>(Method::PUT, "/u/user", &request)
+            .await?
+            .response
+            .uid)
+    }
+
+    /// Removes a user with the supplied user id
+    /// # Arguments
+    /// * `uid` - The `uid` of the user to remove
+    pub async fn user_remove(&self, uid: UID) -> Result<(), VelocityError> {
+        let authkey = self.get_authkey()?;
+
+        let request = UUserDELETEReq {
+            authkey: authkey.key(),
+            uid,
+        };
+
+        self.request(Method::DELETE, "/u/user", &request).await?;
+
+        Ok(())
+    }
+
+    /// Retrieves user information about the user
+    /// # Arguments
+    /// * `uid` - The `uid`, or None for the user that is authenticated by the current authkey
+    pub async fn user_info(&self, uid: Option<UID>) -> Result<UserInfo, VelocityError> {
+        let authkey = self.get_authkey()?;
+
+        let request = UUserPOSTReq {
+            authkey: authkey.key(),
+            uid,
+        };
+
+        Ok(self
+            .request_json::<UUserPOSTReq, UserInfo>(Method::POST, "/u/user", &request)
+            .await?
+            .response)
+    }
+
+    /// Lists all users on a velocity instance
+    pub async fn user_list(&self) -> Result<Vec<UUserListPOSTRes>, VelocityError> {
+        let authkey = self.get_authkey()?;
+
+        let request = UUserListPOSTReq {
+            authkey: authkey.key(),
+        };
+
+        #[derive(Deserialize)]
+        struct Res {
+            users: Vec<UUserListPOSTRes>,
+        }
+
+        Ok(self
+            .request_json::<UUserListPOSTReq, Res>(Method::POST, "/u/user/list", &request)
+            .await?
+            .response
+            .users)
+    }
+}
+
+/// A struct providing information about a user
+#[derive(Deserialize, Debug)]
+pub struct UserInfo {
+    pub uid: UID,
+    pub name: String,
+    pub memberships: Vec<Membership>,
+}
+
+/// Describes a membership
+#[derive(Deserialize, Debug)]
+pub struct Membership {
+    pub gid: GID,
+    pub parent_gid: GID,
+    pub name: String,
+    pub permissions: Vec<Permission>,
+}
+
+/// A permission
+#[derive(Deserialize, Debug)]
+pub struct Permission {
+    pub pid: PID,
+    pub name: String,
+    pub description: String,
+}
+
+/// `/u/user - POST` Request structure
+#[derive(Serialize)]
+struct UUserPOSTReq<'a> {
+    authkey: &'a str,
+    uid: Option<UID>,
+}
+
+/// `/u/user - PUT` Request structure
+#[derive(Serialize)]
+struct UUserPUTReq<'a> {
+    authkey: &'a str,
+    name: &'a str,
+    password: &'a str,
+}
+
+/// `/u/user - PUT` Response structure
+#[derive(Deserialize, Debug)]
+pub struct UUserPUTRes {
+    pub uid: UID,
+    pub name: String,
+}
+
+/// `/u/user - DELETE` Request structure
+#[derive(Serialize)]
+struct UUserDELETEReq<'a> {
+    authkey: &'a str,
+    uid: UID,
+}
+
+/// `/u/user/list - POST` Request structure
+#[derive(Serialize, Debug)]
+struct UUserListPOSTReq<'a> {
+    authkey: &'a str,
+}
+
+/// `/u/user/list - POST` Response structure
+#[derive(Deserialize, Debug)]
+pub struct UUserListPOSTRes {
+    pub uid: UID,
+    pub name: String,
+}
