332ae2af2ceaba31365ea710f0b6e62735d71ba2
Max Kofler <kofler.max.dev@gmail.com>
2023-10-17 16:27:58 +0200

    Initial infrastructure for CLI interface using clik

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
-pub fn main() {
-    println!("[vcmd]");
+use rustyline::DefaultEditor;
+use std::{error::Error, io::Write};
+use velocity::*;
+
+async fn run(args: &Vec<String>) -> Result<(), Box<dyn Error>> {
+    if args.len() != 3 {
+        println!("Usage: {} <Velocity hypervisor URL> <username>", args[0]);
+        return Ok(());
+    }
+
+    // Create a new rustyline editor for reading in history
+    let mut readline = DefaultEditor::new()?;
+
+    // Read in the password
+    print!("Password for {}: ", args[2]);
+    std::io::stdout().flush()?;
+    let password = rpassword::read_password()?;
+
+    // Log in
+    let velocity = Velocity::new(&args[1], &args[2], &password).await?;
+    println!("Logged in as {}", args[2]);
+
+    let mut cli = clik::CLI::new(velocity);
+
+    println!("\n------ vCMD ------\n{}", cli);
+
+    // Handle all incoming lines
+    loop {
+        match readline.readline("vCMD >> ") {
+            Ok(line) => {
+                readline.add_history_entry(&line).unwrap();
+
+                // Handle the line using the CLI struct and respond to errors
+                match cli.handle_async(&line).await {
+                    Ok(_) => {}
+                    Err(e) => println!("ERROR: {e}"),
+                }
+            }
+            Err(_) => break,
+        }
+    }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    match run(&std::env::args().collect()).await {
+        Ok(_) => {}
+        Err(e) => println!("{}", e),
+    }
 }
